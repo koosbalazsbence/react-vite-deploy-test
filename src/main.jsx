4f61c21b5e66f41df424465975607a789ff53293
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-09-24 14:57:21 +0200

    add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import Contact from "./pages/Contact.jsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/react-vite-deploy-test/",
+    element: <App />,
+    children: [
+      {
+        path: "/react-vite-deploy-test/",
+        element: <Home />,
+      },
+      {
+        path: "/react-vite-deploy-test/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+])
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
